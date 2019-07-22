f38e4e75b463491624e5ebd9a7e5887559ae0f47
Paulo Filho <pcf.feat@gmail.com>
2019-07-22 09:14:11 -0400

    three different ways of passing parameters to the server

--- gonode/module1/index.js
+++ gonode/module1/index.js
@@ -4,6 +4,10 @@ const express = require("express");
 // fires up an express server
 const server = express();
 
+// query params = ?test=1
+// route params = /test/1
+// body params = { "name": "Joseph" }
+
 // catches incoming requests on route '/test'
 server.get("/test", (req, res) => {
   // returns json object containing a string
