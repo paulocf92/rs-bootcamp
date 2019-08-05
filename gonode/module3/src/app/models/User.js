edb915913ac1841c8973a4cc6632859e18510132
Paulo Filho <pcf.feat@gmail.com>
2019-08-05 16:39:09 -0400

    feat: enforcing user to avatar file association through foreign key

--- gonode/module3/src/app/models/User.js
+++ gonode/module3/src/app/models/User.js
@@ -25,6 +25,11 @@ class User extends Model {
     return this;
   }
 
+  // associate foreign key from this user to files table
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
