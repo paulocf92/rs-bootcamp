bf3dc9502c71c72f2506b3224f334602a2b70d24
Paulo Filho <pcf.feat@gmail.com>
2019-08-29 09:40:38 -0400

    feat: listing available times for a specific provider

---
+++ gonode/module3/src/app/controllers/AvailableController.js
@@ -0,0 +1,78 @@
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
+    }
+
+    // parse numeric date from front-end to number
+    const searchDate = Number(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    // work hours (only full hours HH:00)
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      /**
+       * set* functions modify some part of timestamp date and returns a new
+       * date.
+       * Order below follows: 1) change hour of passed date, 2) change minute of
+       * returned date, 3) change second of returned date. It only accounts for
+       * full hours (HH:00) as per available schedule list.
+       */
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          // if scheduled appointment's after this time and it's not been picked
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
+  }
+}
+
+export default new AvailableController();
