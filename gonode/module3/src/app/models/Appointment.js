38f3bf51b5756a5904b15e172a1b7465ea517f58
Paulo Filho <pcf.feat@gmail.com>
2019-08-30 14:05:21 -0400

    feat: adding variable virtual fields

--- gonode/module3/src/app/models/Appointment.js
+++ gonode/module3/src/app/models/Appointment.js
@@ -1,4 +1,5 @@
 import { Model, Sequelize } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -6,6 +7,20 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        // virtual field evaluated on the fly
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          // is this appointment within its cancelable timeframe?
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         sequelize,
