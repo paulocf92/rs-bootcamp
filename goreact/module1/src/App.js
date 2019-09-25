0c89328ae848a013be9666532992b1e3f024d826
Paulo Filho <pcf.feat@gmail.com>
2019-09-25 13:06:08 -0400

    app: handling state management with class components

--- goreact/module1/src/App.js
+++ goreact/module1/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import profile from "./assets/boy.png";
+import TechList from "./components/TechList";
 
 function App() {
-  return <img width="400" src={profile} />;
+  return <TechList />;
 }
 
 export default App;
