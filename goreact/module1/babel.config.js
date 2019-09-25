e7c0d71919b782fc927858aaef40ff1ede54af51
Paulo Filho <pcf.feat@gmail.com>
2019-09-25 11:08:16 -0400

    feat: babel/webpack basic settings, running dev server with webpack

---
+++ goreact/module1/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",   // adds support for ES6+ syntax and features
+    "@babel/preset-react", // adds support for react-specific syntax/features
+  ]
+}
