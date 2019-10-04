bc366296068864c441a5be6226af4bf0d5f8e11b
Paulo Filho <pcf.feat@gmail.com>
2019-10-04 08:49:52 -0400

    feat: styled components with custom props handling

--- goreact/module2/src/pages/Main/index.js
+++ goreact/module2/src/pages/Main/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Title } from './styles';
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Title error={false}>
+      Main<small>menor</small>
+    </Title>
+  );
 }
