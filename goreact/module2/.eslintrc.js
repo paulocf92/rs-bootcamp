4284037d9ab3c17fb05a389a526eac9da026583f
Paulo Filho <pcf.feat@gmail.com>
2019-10-09 10:27:51 -0400

    feat: setting prop types

--- goreact/module2/.eslintrc.js
+++ goreact/module2/.eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
   },
 };
