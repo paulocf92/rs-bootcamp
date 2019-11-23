31cc3caf8f986d1659dd764bbc62622222d68583
Paulo Filho <pcf.feat@gmail.com>
2019-11-23 11:31:48 -0400

    feat: drafting base appearance with React Navigation

--- gonative/module1/src/index.js
+++ gonative/module1/src/index.js
@@ -1,29 +1,8 @@
 import React from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
-
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    color: '#333333',
-    margin: 10,
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!!</Text>
-    </View>
-  );
+  return <Routes />;
 }
