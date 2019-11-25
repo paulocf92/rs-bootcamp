e4eb83b764db80ebbe96a77de7146d74b039d5df
Paulo Filho <pcf.feat@gmail.com>
2019-11-25 11:04:27 -0400

    feat: styling form with styled components and material icons for RN

--- gonative/module1/src/pages/Main/index.js
+++ gonative/module1/src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
