95c99108c91fc409af023966e728d8fe7ef0e9f7
Paulo Filho <pcf.feat@gmail.com>
2019-11-26 15:23:37 -0400

    feat: retrieving starred repositories for User page, prop types validation

--- gonative/module1/src/pages/User/index.js
+++ gonative/module1/src/pages/User/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
-  return <View />;
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
