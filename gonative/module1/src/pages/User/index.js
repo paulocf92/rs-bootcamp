38a4c7b5d0bc599d6951ecdeea9014a057d1a37f
Paulo Filho <pcf.feat@gmail.com>
2019-11-26 15:58:17 -0400

    feat: starred repositories list

--- gonative/module1/src/pages/User/index.js
+++ gonative/module1/src/pages/User/index.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.getParam('user').name,
   });
 
-  state = {
-    stars: [],
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      stars: [],
+    };
+  }
 
   async componentDidMount() {
     const { navigation } = this.props;
@@ -24,8 +39,33 @@ export default class User extends Component {
   }
 
   render() {
+    const { navigation } = this.props;
     const { stars } = this.state;
-    return <View />;
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
 
