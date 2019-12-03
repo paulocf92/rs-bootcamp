28acc1b28d32f368b21434563119da08f41718e1
Paulo Filho <pcf.feat@gmail.com>
2019-12-03 08:32:18 -0400

    feat: creating app Header

--- fundamentals/redux/src/pages/Cart/index.js
+++ fundamentals/redux/src/pages/Cart/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return <h1 style={{ color: '#fff' }}>Carrinho</h1>;
 }
