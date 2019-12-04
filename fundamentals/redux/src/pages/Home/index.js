26e879ad9f975f56529716d0459ea8f5816c96f3
Paulo Filho <pcf.feat@gmail.com>
2019-12-04 16:54:44 -0400

    feat: managing and displaying amounts in Home

--- fundamentals/redux/src/pages/Home/index.js
+++ fundamentals/redux/src/pages/Home/index.js
@@ -37,6 +37,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -50,7 +51,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                <MdAddShoppingCart size={16} color="#FFF" />{' '}
+                {amount[product.id] || 0}
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
@@ -62,9 +64,16 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 // Bind Cart Actions as component props
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 // Redux(1): Connect component to store
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
