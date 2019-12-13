701904dafb0457367f891d749c94af7203b21759
Paulo Filho <pcf.feat@gmail.com>
2019-12-13 14:54:48 -0400

    feat: using useState instead of traditional class state approach

--- stage4/module9/src/App.js
+++ stage4/module9/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [techs, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button onClick={handleAdd}>Adicionar</button>
+    </>
+  );
 }
 
 export default App;
