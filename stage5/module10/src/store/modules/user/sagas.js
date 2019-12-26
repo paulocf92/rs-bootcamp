8cc6995cfd46238c86b3509ca0fbfd654c16eb46
Paulo Filho <pcf.feat@gmail.com>
2019-12-26 15:59:46 -0400

    feat: updating profile image

--- stage5/module10/src/store/modules/user/sagas.js
+++ stage5/module10/src/store/modules/user/sagas.js
@@ -7,9 +7,14 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
-    const profile = { name, email, ...(rest.oldPassord ? rest : {}) };
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassord ? rest : {}),
+    };
 
     const response = yield call(api.put, 'users', profile);
 
