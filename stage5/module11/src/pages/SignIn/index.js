fdce00e765d948828a32410bb3c0dfba38071e35
Paulo Filho <pcf.feat@gmail.com>
2020-01-20 16:57:41 -0400

    feat: input and button components

--- stage5/module11/src/pages/SignIn/index.js
+++ stage5/module11/src/pages/SignIn/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
