7cbfd382b95090eca41a747fb27fd4731cda0ada
Paulo Filho <pcf.feat@gmail.com>
2020-01-20 16:03:29 -0400

    feat: laying out app background

--- stage5/module11/src/pages/SignUp/index.js
+++ stage5/module11/src/pages/SignUp/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
 export default function SignUp() {
-  return <View />;
+  return (
+    <Background>
+      <Text>SignUp</Text>
+    </Background>
+  );
 }
