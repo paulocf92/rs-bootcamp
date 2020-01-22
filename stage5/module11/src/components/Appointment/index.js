1f71a92216fe4f9f07928e174697a63f42d2deca
Paulo Filho <pcf.feat@gmail.com>
2020-01-22 09:34:22 -0400

    feat: appointments list

---
+++ stage5/module11/src/components/Appointment/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment() {
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{ uri: 'https://api.adorable.io/avatars/50/rocketseat.png' }}
+        />
+
+        <Info>
+          <Name>Paulo Filho</Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" size={20} color="#F64c75" />
+      </TouchableOpacity>
+    </Container>
+  );
+}
