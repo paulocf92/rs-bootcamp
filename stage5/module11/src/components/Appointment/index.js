7fe4b199dc7c4aac61b6f08ad565ea59dd157344
Paulo Filho <pcf.feat@gmail.com>
2020-01-22 11:00:25 -0400

    feat: appointments obtained through api

--- stage5/module11/src/components/Appointment/index.js
+++ stage5/module11/src/components/Appointment/index.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointment() {
+export default function Appointment({ data, onCancel }) {
+  const avatarUrl = useMemo(() => {
+    return data.provider.avatar
+      ? data.provider.avatar.url.replace('localhost', '192.168.1.175')
+      : null;
+  }, [data.provider.avatar]);
+
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
         <Avatar
-          source={{ uri: 'https://api.adorable.io/avatars/50/rocketseat.png' }}
+          source={{
+            uri: data.provider.avatar
+              ? avatarUrl
+              : `https://api.adorable.io/avatars/50/${data.provider.name}`,
+          }}
         />
 
         <Info>
-          <Name>Paulo Filho</Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#F64c75" />
-      </TouchableOpacity>
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#F64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 }
+
+Appointment.propTypes = {
+  data: PropTypes.shape({
+    provider: PropTypes.shape({
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+      name: PropTypes.string,
+    }),
+    date: PropTypes.string,
+    past: PropTypes.bool,
+    cancelable: PropTypes.bool,
+    canceled_at: PropTypes.string,
+  }).isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
