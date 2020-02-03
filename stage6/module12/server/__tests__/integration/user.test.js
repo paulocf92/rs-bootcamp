e4617b2391dd5a8f40cac4e1e2068065bd080e91
Paulo Filho <pcf.feat@gmail.com>
2020-02-03 13:59:01 -0400

    feat: add duplicate email validation, password hash comparison validation

--- stage6/module12/server/__tests__/integration/user.test.js
+++ stage6/module12/server/__tests__/integration/user.test.js
@@ -1,16 +1,56 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Paulo Filho',
+      email: 'paulo@gmail.com',
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Paulo Filho',
         email: 'paulo@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Paulo Filho',
+        email: 'paulo@gmail.com',
+        password: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Paulo Filho',
+        email: 'paulo@gmail.com',
+        password: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
